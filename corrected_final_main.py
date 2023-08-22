a854923fac347081ba155bfa0a5947cf3ef7d000
KayKilb <115676011+KayKilb@users.noreply.github.com>
2023-08-22 10:14:25 -0500

    corrected file indention to run game

--- corrected_final_main.py
+++ corrected_final_main.py
@@ -75,20 +75,20 @@ while True:
     draw_board()
 
     # Display winner if there is one
-    if winner:
-        winner_text = font.render(f"{winner} WINS!!!", True, (255, 0, 0))
-        screen.blit(winner_text, (SCREEN_WIDTH // 2 - winner_text.get_width() // 2, SCREEN_HEIGHT // 2 - winner_text.get_height() // 2))
+if winner:
+    winner_text = font.render(f"{winner} WINS!!!", True, (255, 0, 0))
+    screen.blit(winner_text, (SCREEN_WIDTH // 2 - winner_text.get_width() // 2, SCREEN_HEIGHT // 2 - winner_text.get_height() // 2))
 
     pygame.display.flip()
     clock.tick(60)
 
-        exit()
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            x, y = pygame.mouse.get_pos()
-            row, col = x // CELL_SIZE, y // CELL_SIZE
-            if board[col][row] == '':
-                board[col][row] = 'O' if player_turn == 'O' else 'X'
-                player_turn = 'O' if player_turn == 'X' else 'X'
+    exit()
+elif event.type == pygame.MOUSEBUTTONDOWN:
+    x, y = pygame.mouse.get_pos()
+    row, col = x // CELL_SIZE, y // CELL_SIZE
+    if board[col][row] == '':
+        board[col][row] = 'O' if player_turn == 'O' else 'X'
+        player_turn = 'O' if player_turn == 'X' else 'X'
 
     # Draw everything
     screen.blit(background_image, (0, 0))
